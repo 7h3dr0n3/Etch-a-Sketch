15295145425fea05a394c61c11c9ef9c0ceb72d4
dr0n3 <7h3dr0n3@gmail.com>
2022-12-29 21:42:31 +0530

    Refractor and add func createSquare(), newGrid(), changeColor()
    Add a button to the top of the screen that will send the user a popup
    asking for the number of squares per side for the new grid. Once
    entered, the existing grid should be removed and a new grid should
    be generated

--- main.js
+++ main.js
@@ -1,41 +1,70 @@
-// create 16x16 grid of square div's
-
 const container = document.getElementById("container");
+let rows = 16, cols = 16;
 
-for (let i = 0; i < 256; i++) {
-    const node = document.createElement('div');
-    node.classList.add('square');
-    container.appendChild(node);
-}
+//callbacks with default
+newGrid(rows, cols);
+changeColor();
 
-// change div's color when your mouse passes over them
-const square = document.querySelectorAll('.square');
-
-square.forEach(element => {
-    element.addEventListener("mouseover", (event) => {
-        const toRedSquare = event.target;
-        toRedSquare.classList.add('red');
-
-    });
-});
-
-// ask no of square (max=100)per side for the new grid,
-// existing grid should be removed and a new grid should be generated 
-
+//insert button at top and event listener
 const button = document.createElement('button');
 button.innerText = "Set new Square/side";
 document.body.insertBefore(button, container);
 
+
+// create 16x16 grid of square div's
+function createSquare(rows, cols) {
+
+    for (let i = 0; i < rows * cols; i++) {
+        const node = document.createElement('div');
+        node.classList.add('square');
+        node.innerText = `${i + 1}`;
+        container.appendChild(node);
+    }
+
+};
+
+
+//create new grid
+function newGrid(rows, cols) {
+    let auto = "";
+    for (let i = 0; i < rows; i++) {
+        let prop = "auto "
+        auto += prop;
+    }
+    // console.log(auto);
+    container.style.gridTemplateColumns = auto;
+    container.style.gridTemplateRows = auto;
+    createSquare(rows, cols);
+};
+
+
+// change div's color when your mouse passes over them
+function changeColor() {
+    const square = document.querySelectorAll('.square');
+    square.forEach(element => {
+        element.addEventListener("mouseover", (event) => {
+            const toRedSquare = event.target;
+            toRedSquare.classList.add('colored');
+
+        });
+    });
+
+};
+
+//hover listener with callbacks
+
 button.addEventListener('click', (event) => {
+
     const gridSquares = prompt("Enter No of Squares per Side?", 16);
     if (gridSquares > 100) {
-        gridSquares = prompt("Enter value should be less than 100? ", 16);
+        gridSquares = prompt("Enter numeric value from 0 to 100? ", 16);
     }
+    rows = gridSquares;
+    cols = gridSquares;
+
+    const allPrevSquares = document.querySelectorAll('.square');
+    allPrevSquares.forEach(element => element.remove());
+
+    newGrid(rows, cols);
+    changeColor()
 });
-
-
-
-
-
-
-
