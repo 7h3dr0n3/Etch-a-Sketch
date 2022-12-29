83af6b69c06050ece6b48e8d6ecb22883b0ccb02
dr0n3 <7h3dr0n3@gmail.com>
2022-12-29 18:16:52 +0530

    Add event listener on .square divs and hover effect to change color
    add mouseover event listener using forEach loop that the grid divs
    change color when your mouse passes over them pixelated style

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(node);
 }
 
+
+const square = document.querySelectorAll('.square');
+
+console.log(square);
+square.forEach(element => {
+    element.addEventListener("mouseover", (event) => {
+        const toRedSquare = event.target;
+        toRedSquare.classList.add('red');
+        // console.log(toRedSquare);
+    });
+});
