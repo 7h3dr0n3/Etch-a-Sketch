cc9b508eed678f76b30743a3b2c818ec1ce40b8b
dr0n3 <7h3dr0n3@gmail.com>
2022-12-29 19:05:37 +0530

    Add new button at top before container

--- main.js
+++ main.js
@@ -3,21 +3,24 @@
 const container = document.getElementById("container");
 
 for (let i = 0; i < 256; i++) {
-
     const node = document.createElement('div');
     node.classList.add('square');
-
     container.appendChild(node);
 }
 
-
+// change div's color when your mouse passes over them
 const square = document.querySelectorAll('.square');
 
-console.log(square);
 square.forEach(element => {
     element.addEventListener("mouseover", (event) => {
         const toRedSquare = event.target;
         toRedSquare.classList.add('red');
-        // console.log(toRedSquare);
+
     });
 });
+
+//add button at top of page
+
+const button = document.createElement('button');
+button.innerText = "Set new Square/side";
+document.body.insertBefore(button, container);
